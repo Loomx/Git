16b017d3ce0f4fa723f73afb1b9479caa74cb73d
Loomx <jonny@kingslea.school.nz>
2013-11-23 20:06:22 +1300

    Change `isfloating' from true to false for Gimp

--- dwm-6.0/config.h
+++ dwm-6.0/config.h
@@ -19,7 +19,7 @@ static const char *tags[] = { "Here", "There" };
 
 static const Rule rules[] = {
     /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            True,        -1 },
+    { "Gimp",     NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
