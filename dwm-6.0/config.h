3c228464ce28d7cc9f41f07ee34440a89bd7d921
Loomx <jonny@kingslea.school.nz>
2014-03-12 16:28:01 +1300

    Use `instance' instead of `title' in rules[]

--- dwm-6.0/config.h
+++ dwm-6.0/config.h
@@ -19,7 +19,7 @@ static const char *tags[] = { "Here", "There" };
 
 static const Rule rules[] = {
     /* class      instance    title       tags mask     isfloating   monitor */
-    {  NULL,      NULL,       "xv",       0,            True,        -1 },
+    {  NULL,      "xv",       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */
