b0dc94150f74a5e0903470f2ba91ebf575a32b77
Loomx <jonny@kingslea.school.nz>
2014-04-30 15:17:43 +1200

    Change `selfgcolor' to #22bbee

--- dmenu-4.5/dmenu.c
+++ dmenu-4.5/dmenu.c
@@ -48,7 +48,7 @@ static const char *prompt = NULL;
 static const char *normbgcolor = "#222222";
 static const char *normfgcolor = "#bbbbbb";
 static const char *selbgcolor  = "#222222";    // #005577
-static const char *selfgcolor  = "#429eee";    // #eeeeee
+static const char *selfgcolor  = "#22bbee";    // #eeeeee
 static unsigned int lines = 0;
 static unsigned long normcol[ColLast];
 static unsigned long selcol[ColLast];
