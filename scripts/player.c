7a7910aa261c7e7940d8c0133b2f41c9a27f13f6
Jonny Langley <jonny@kingslea.school.nz>
2014-11-27 16:26:53 +1300

    Add pseudocode version of player.c

---
+++ scripts/player.c
@@ -0,0 +1,14 @@
+/*
+ * Check cache files and update if needed
+ *
+ * Open dmenu to choose an album
+ *
+ * Open dmenu to prompt for filters | trackname
+ *
+ * Start mplayer with tracklist
+ *
+ * Set up loop whle mplayer is running to update track name for dstatus
+ *
+ * Clean up when mplayer exits
+ *
+ */
