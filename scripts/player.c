c16369b850bdae55a80aca51ab47c7b6596bfc9e
Jonny <jonny@kingslea.school.nz>
2014-11-27 21:51:36 +1300

    Setup player.c

--- scripts/player.c
+++ scripts/player.c
@@ -1,14 +1,26 @@
-/*
- * Check cache files and update if needed
- *
- * Open dmenu to choose an album
- *
- * Open dmenu to prompt for filters | trackname
- *
- * Start mplayer with tracklist
- *
- * Set up loop whle mplayer is running to update track name for dstatus
- *
- * Clean up when mplayer exits
- *
- */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define ALBUMCACHE "/home/jonny/.mplayer/album_cache"
+#define TRACKCACHE "/home/jonny/.mplayer/track_cache"
+#define MPOUTPUT   "/tmp/mp_output"
+#define STATUSMSG  "/tmp/status_msg"
+
+int
+main(void) {
+
+    /* Check cache files and update if needed */
+
+    /* Open dmenu to choose an album */
+
+    /* Open dmenu to prompt for filters | trackname */
+
+    /* Start mplayer with tracklist */
+
+    /* Set up loop whle mplayer is running to update track name for dstatus */
+
+    /* Clean up when mplayer exits */
+
+}
