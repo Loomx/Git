11cf26eca21b36d8789e9cf2045f36e5e2232e51
Jonny Langley <jonny@kingslea.school.nz>
2014-12-01 20:50:59 +1300

    Save good dmenu_cache.c

--- dmenu-5/dmenu_cache.c
+++ dmenu-5/dmenu_cache.c
@@ -7,33 +7,34 @@
 
 #define CACHE  ".cache/dmenu_run"
 
+static void eprintf(const char *s);
+
 static int qstrcmp(const void *a, const void *b);
-static void die(const char *s);
 static void scan(void);
 static int uptodate(void);
 
-static char **bins = NULL;
 static const char *HOME, *PATH;
-static size_t count = 0;
 
 int
 main(void) {
 	if(!(HOME = getenv("HOME")))
-		die("no $HOME");
+		eprintf("no $HOME");
 	if(!(PATH = getenv("PATH")))
-		die("no $PATH");
+		eprintf("no $PATH");
 	if(chdir(HOME) < 0)
-		die("chdir failed");
+		eprintf("chdir failed");
 	if(!(uptodate()))
 	    scan();
 	return EXIT_SUCCESS;
 }
 
+/*
 void
-die(const char *s) {
+eprintf(const char *s) {
 	fprintf(stderr, "dmenu_path: %s\n", s);
 	exit(EXIT_FAILURE);
 }
+*/
 
 int
 qstrcmp(const void *a, const void *b) {
@@ -43,14 +44,14 @@ qstrcmp(const void *a, const void *b) {
 void
 scan(void) {
 	char buf[PATH_MAX];
-	char *dir, *path;
-	size_t i;
+	char *dir, *path, **bins = NULL;
+	size_t i, count = 0;
 	struct dirent *ent;
 	DIR *dp;
 	FILE *cache;
 
 	if(!(path = strdup(PATH)))
-		die("strdup failed");
+		eprintf("strdup failed");
 	for(dir = strtok(path, ":"); dir; dir = strtok(NULL, ":")) {
 		if(!(dp = opendir(dir)))
 			continue;
@@ -59,19 +60,19 @@ scan(void) {
 			if(ent->d_name[0] == '.' || access(buf, X_OK) < 0)
 				continue;
 			if(!(bins = realloc(bins, ++count * sizeof *bins)))
-				die("malloc failed");
+				eprintf("malloc failed");
 			if(!(bins[count-1] = strdup(ent->d_name)))
-				die("strdup failed");
+				eprintf("strdup failed");
 		}
 		closedir(dp);
 	}
 	qsort(bins, count, sizeof *bins, qstrcmp);
 	if(!(cache = fopen(CACHE, "w")))
-		die("open failed");
+		eprintf("open failed");
 	for(i = 0; i < count; i++) {
 		if(i > 0 && !strcmp(bins[i], bins[i-1]))
 			continue;
-		fprintf(cache,  "%s\n", bins[i]);
+		fprintf(cache, "%s\n", bins[i]);
 	}
 	fclose(cache);
 	free(path);
@@ -87,7 +88,7 @@ uptodate(void) {
 		return 0;
 	mtime = st.st_mtime;
 	if(!(path = strdup(PATH)))
-		die("strdup failed");
+		eprintf("strdup failed");
 	for(dir = strtok(path, ":"); dir; dir = strtok(NULL, ":"))
 		if(!stat(dir, &st) && st.st_mtime > mtime)
 			return 0;
