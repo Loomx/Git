66ca795b3760b5e26e79f4a6a6ec07e6239c9500
Jonny <jonny@kingslea.school.nz>
2014-12-19 23:44:17 +1300

    Start on scan()

--- scripts/player.c
+++ scripts/player.c
@@ -12,7 +12,6 @@
 #define MPOUTPUT   "/tmp/mp_output"
 #define STATUSMSG  "/tmp/status_msg"
 
-static void chomp(char *s);
 static void eprintf(const char *s);
 static int qstrcmp(const void *a, const void *b);
 static void scan(void);
@@ -28,9 +27,12 @@ main(void) {
 		eprintf("no $HOME");
 	if(chdir(HOME) < 0)
 		eprintf("chdir failed");
+
 	if(!uptodate())
-	    //scan();
+    {
+	    scan();
         printf("scanning...\n");
+    }
 
     /* Open dmenu to choose an album */
 
@@ -58,12 +60,26 @@ qstrcmp(const void *a, const void *b) {
 void
 scan(void) {
 	char buf[PATH_MAX];
-	char *dir, *path, **bins = NULL;
+	//char *dir, *path, **token = NULL;
+	char **token = NULL;
 	size_t i, count = 0;
 	struct dirent *ent;
 	DIR *dp;
 	FILE *cache;
 
+    dp = opendir(MUSICDIR);
+    while((ent = readdir(dp))) {
+        snprintf(buf, sizeof buf, "%s", ent->d_name);
+        if(ent->d_name[0] == '.')
+            continue;
+        if(!(token = realloc(token, ++count * sizeof *token)))
+            eprintf("malloc failed");
+        if(!(token[count-1] = strdup(ent->d_name)))
+            eprintf("strdup failed");
+    }
+    closedir(dp);
+
+    /*
 	for(dir = strtok(path, ":"); dir; dir = strtok(NULL, ":")) {
 		if(!(dp = opendir(dir)))
 			continue;
@@ -71,23 +87,25 @@ scan(void) {
 			snprintf(buf, sizeof buf, "%s/%s", dir, ent->d_name);
 			if(ent->d_name[0] == '.' || access(buf, X_OK) < 0)
 				continue;
-			if(!(bins = realloc(bins, ++count * sizeof *bins)))
+			if(!(token = realloc(token, ++count * sizeof *token)))
 				eprintf("malloc failed");
-			if(!(bins[count-1] = strdup(ent->d_name)))
+			if(!(token[count-1] = strdup(ent->d_name)))
 				eprintf("strdup failed");
 		}
 		closedir(dp);
 	}
-	qsort(bins, count, sizeof *bins, qstrcmp);
-	if(!(cache = fopen(TRACKCACHE, "w")))
-		eprintf("open failed");
+    */
+
+	qsort(token, count, sizeof *token, qstrcmp);
+	if(!(cache = fopen(ALBUMCACHE, "w")))
+		eprintf("cache open failed");
 	for(i = 0; i < count; i++) {
-		if(i > 0 && !strcmp(bins[i], bins[i-1]))
+		if(i > 0 && !strcmp(token[i], token[i-1]))
 			continue;
-		fprintf(cache, "%s\n", bins[i]);
+		fprintf(cache, "%s\n", token[i]);
 	}
 	fclose(cache);
-	free(path);
+	//free(path);
 }
 
 int
@@ -98,11 +116,12 @@ uptodate(void) {
 	time_t mtime;
     FILE *cache;
 
+
     cache = fopen(ALBUMCACHE, "r");
 	if(stat(ALBUMCACHE, &st) < 0)
 		return 0;
-
 	mtime = st.st_mtime;
+
     while((getline(&line, &len, cache)) != -1) {
         line[strlen(line) - 1] = '\0';
 		if((stat(line, &st) < 0) || st.st_mtime > mtime) {
