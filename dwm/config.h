b8280b12a005ac31032b431b6ed895158a30072c
Loomx <jonny@kingslea.school.nz>
2022-05-02 11:31:30 +1200

    Remove `ejectcmd' and old volume commands from dwm/config.h

--- dwm/config.h
+++ dwm/config.h
@@ -57,7 +57,6 @@ static const char *nextcmd[]      = { "player", "pt_step", "1", NULL };
 static const char *mutecmd[]      = { "amixer", "-q", "set", "Master", "toggle", NULL };
 static const char *voldowncmd[]   = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
 static const char *volupcmd[]     = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
-static const char *ejectcmd[]     = { "eject", NULL };
 
 static Key keys[] = {
 	/* modifier            key                        function        argument */
@@ -70,9 +69,6 @@ static Key keys[] = {
 	{ 0,                   0x1008ff12,                spawn,          {.v = mutecmd } },
 	{ 0,                   0x1008ff11,                spawn,          {.v = voldowncmd } },
 	{ 0,                   0x1008ff13,                spawn,          {.v = volupcmd } },
-//	{ 0,                   0x1008ff11,                spawn,          SHCMD("amixer set Master 5%- unmute | awk -F [][] 'END { print $2 }' >/tmp/volume") },
-//	{ 0,                   0x1008ff13,                spawn,          SHCMD("amixer set Master 5%+ unmute | awk -F [][] 'END { print $2 }' >/tmp/volume") },
-	{ 0,                   0x1008ff2c,                spawn,          {.v = ejectcmd } },
 	{ 0,                   0x1008ff03,                spawn,          SHCMD("Mon=$(cat /sys/class/backlight/intel_backlight/brightness); echo $((Mon-1920)) >/sys/class/backlight/intel_backlight/brightness") },
 	{ 0,                   0x1008ff02,                spawn,          SHCMD("Mon=$(cat /sys/class/backlight/intel_backlight/brightness); echo $((Mon+1920)) >/sys/class/backlight/intel_backlight/brightness") },
 //	{ 0,                   0x1008ff06,                spawn,          SHCMD("Kbd=$(cat ~/.kbd_brightness); echo $((Kbd-2)) >~/.kbd_brightness") },
