828ddae6d247d9efec85167e151dfdf243e88dbd
Loomx <jonny@kingslea.school.nz>
2022-05-18 21:34:40 +1200

    Use default mouse tag/view commands

--- dwm/config.h
+++ dwm/config.h
@@ -95,7 +95,7 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            ControlMask,    Button1,        toggleview,     {0} },
-	{ ClkTagBar,            0,              Button3,        tag,            {0} },
-	{ ClkTagBar,            ControlMask,    Button3,        toggletag,      {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
